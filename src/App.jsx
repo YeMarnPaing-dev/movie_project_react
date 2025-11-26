e4e0595bb781df6acb80d5ac636441f26d56a371
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-11-26 23:01:00 +0630

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Alert} from 'flowbite-react';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Alert color="info">
+      <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+    </Alert>
+    </>
+  )
+}
+
+export default App
